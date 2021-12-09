c149aac61331a91c07b6bfe3376fdd7839c00143
Luca Bazzea <luca.bazzea@gmail.com>
2021-12-09 16:16:21 +0200

    - Modified .gitignore + py/main.py - Deleted css/style.css + index.html

--- py/main.py
+++ py/main.py
@@ -1,16 +1,55 @@
+from flask.templating import render_template_string
+from flask.wrappers import Response
 import mysql.connector
+from werkzeug.wrappers import response
 from login import user, password # Import login details from git ignored file
+from flask import	Flask, redirect, url_for, render_template, flash, request
 
-db = mysql.connector.connect(
-  host = "localhost",
-  user = user,
-  password = password,
-  database = "site"
-)
+app = Flask(__name__)
 
-mycursor = db.cursor()
+@app.route("/index", methods=["GET","POST"])
+def get_form():
 
-mycursor.execute("INSERT INTO contact (fname, lname, subject, msg, email, date) VALUES ('Luca', 'Bazzea', 'Test', 'text', 'luca@email.com', CURRENT_DATE())") # Insert data from user input
+  error = None
 
-db.commit()
-print("\nTable updated successfully")
+  try:
+    if request.method == 'POST':
+      fname = request.form['fname']
+      lname = request.form['lname']
+      subject = request.form['subject']
+      message = request.form['message']
+      email = request.form['email']
+
+    else:
+      print("get_form() failed")
+
+  except Exception as e:
+    print(e)
+    return render_template("index.html", response = error)
+
+fname, lname, subject, message, email = get_form()
+
+
+print(f"\nName: {fname}\nSurname: {lname}\nSubject: {subject}\nMessage: {message}\nEmail: {email}\n")
+
+# db = mysql.connector.connect(
+#   host = "localhost",
+#   user = user,
+#   password = password,
+#   database = "site"
+# )
+
+# mycursor = db.cursor()
+
+# mycursor.execute(f"INSERT INTO contact (fname, lname, subject, msg, email, date) VALUES ('{fname}', '{lname}', '{subject}', '{message}', '{email}', CURRENT_DATE())") # Insert data from user input
+
+# db.commit()
+
+# print("\nTable updated successfully")
+
+# resp = "null"
+
+# @app.route("/")
+# def Response():
+
+#   return render_template("index.html", response = resp)
